7558193333bb55f2f7924fa2330524c4b929fab6
Ruvindu <ruvindusharada22@gmail.com>
2025-12-21 12:46:30 +0530

    refactor: enhance name handling and output formatting

--- hellow.py
+++ hellow.py
@@ -13,3 +13,14 @@ last_name = "Doe"
 dash="-" * 10
 print(first_name +" " + last_name)
 print(dash)
+
+print(len(first_name) + len(last_name))
+
+
+name ="RUVINDU"
+name.lower()
+print(name.lower())
+print(name.upper())
+
+sententce = "The quick brown fox jumps over the lazy dog"
+print(sententce.title())
