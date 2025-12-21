7a2753db210728a1d7a5489b1f96f9b4a3987c19
Ruvindu <ruvindusharada22@gmail.com>
2025-12-21 21:32:36 +0530

    refactor: comment out unused code and maintain structure

--- hellow.py
+++ hellow.py
@@ -1,26 +1,47 @@
-import requests
+# import requests
 
 
-response = requests.get('https://api.github.com')
-print(response.status_code)
+# response = requests.get('https://api.github.com')
+# print(response.status_code)
 
 
 
-first_name = "John"
-last_name = "Doe"
+# first_name = "John"
+# last_name = "Doe"
 
 
-dash="-" * 10
-print(first_name +" " + last_name)
-print(dash)
+# dash="-" * 10
+# print(first_name +" " + last_name)
+# print(dash)
 
-print(len(first_name) + len(last_name))
+# print(len(first_name) + len(last_name))
 
 
-name ="RUVINDU"
-name.lower()
-print(name.lower())
-print(name.upper())
+# name ="RUVINDU"
+# name.lower()
+# print(name.lower())
+# print(name.upper())
 
-sententce = "The quick brown fox jumps over the lazy dog"
-print(sententce.title())
+# sententce = "The quick brown fox jumps over the lazy dog"
+# print(sententce.title())
+
+
+# temprage_celsius = 70
+
+# if temprage_celsius > 30:
+#     print("It's a hot day")
+# elif temprage_celsius > 20:
+#     print("It's a warm day")
+# else:
+#     print("It's a cold day")
+
+
+for i in range(5):
+    print(i)
+print("Hello, World!")
+
+customer=[1,"hii",4,5,6]
+
+customer[0]="Buddima"
+customer.append("New Value")
+print(customer)
