e56df4fb9649a7be530c9664f1c93769a8898fc3
Ruvindu <ruvindusharada22@gmail.com>
2025-12-21 12:28:27 +0530

    refactor: reorganize code structure and add name printing functionality

--- hellow.py
+++ hellow.py
@@ -3,3 +3,13 @@ import requests
 
 response = requests.get('https://api.github.com')
 print(response.status_code)
+
+
+
+first_name = "John"
+last_name = "Doe"
+
+
+dash="-" * 10
+print(first_name +" " + last_name)
+print(dash)
