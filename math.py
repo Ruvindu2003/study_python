d736669aaab0bf7314dedfc16122ab5bf4e57a45
Ruvindu <ruvindusharada22@gmail.com>
2025-12-29 04:05:50 +0530

    feat: add initial math operations and random selection functionality

---
+++ math.py
@@ -0,0 +1,18 @@
+import math 
+import random 
+import json
+
+print(math.sqrt(16))
+
+name=["Alice", "Bob", "Charlie", "Diana"]
+numbers=[0,1,2,3,4,5,6,7,8,9]
+random_name=random.choice(name)
+print(random_name)
+
+numberss=random.choice(numbers)
+print(numberss)
+
+
+name_dict={"name":"Buddima","age":22,"city":"Colombo"}
+name_json=json.dumps(name_dict)
+print(name_json)
